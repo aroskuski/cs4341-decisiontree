35fb5fb319240faa2eb5d7c2c954aa286958ed00
Andrew Roskuski <aroskuski@gmail.com>
2015-02-04 19:57:05 -0500

    First Commit

---
+++ src/CS4341-DecisionTree.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : CS4341-DecisionTree.cpp
+// Author      : Andrew Roskuski & Connor Porell
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
